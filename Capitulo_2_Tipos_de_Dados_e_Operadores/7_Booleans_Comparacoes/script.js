9f565beefeaa51cfce48abdc6176415e5e48b1a1
Hector <150470219+W0lfzZera@users.noreply.github.com>
2024-04-24 20:57:52 -0300

    Add files via upload

---
+++ Capitulo_2_Tipos_de_Dados_e_Operadores/7_Booleans_Comparacoes/script.js
@@ -0,0 +1,8 @@
+console.log(1 > 2);
+console.log(5 < 10);
+console.log(3 >= 3);
+console.log(5 <= 4);
+console.log(5 == 4);
+console.log('Hector' != 'Hector');
+console.log(3 === '3');
+console.log(4 === 4);
---
+++ Capitulo_2_Tipos_de_Dados_e_Operadores/9_Booleans_Operador_Ternario/script.js
@@ -0,0 +1,3 @@
+console.log(5 > 2 ? 'É sim' : 'É não');
+console.log(false ? 5 : 4);
+console.log("Hector" == "Hector" ? "Olá, Hector!" : "Não é o Hector!");
