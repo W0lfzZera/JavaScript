2a5776fd3004565f2546c4a8839bfd8cd24e8224
Hector <150470219+W0lfzZera@users.noreply.github.com>
2024-04-24 21:00:32 -0300

    Add files via upload

---
+++ Capitulo_6_Funcoes/1_Definir_Funcoes/script.js
@@ -0,0 +1,21 @@
+function imprimirNoConsole() {
+  console.log("Hello World!");
+}
+
+imprimirNoConsole();
+
+function imprimirUmNumero(numero) {
+  console.log("O número é: " + numero);
+}
+
+imprimirUmNumero(2);
+imprimirUmNumero(4);
+imprimirUmNumero(6);
+imprimirUmNumero(8);
+
+const numeroAleatorio = function() {
+  console.log(Math.random());
+};
+
+numeroAleatorio();
+numeroAleatorio();
---
+++ Capitulo_6_Funcoes/3_Escopo_da_Funcao/script.js
@@ -0,0 +1,11 @@
+let x = 10;
+
+function imprimir() {
+  let x = 150;
+
+  console.log(x);
+}
+
+imprimir();
+
+console.log(x);
